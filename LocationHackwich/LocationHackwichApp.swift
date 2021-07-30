4957f89ca39277f3dbd3b0a6b3f48471aeec18ca
Tucker Gillbrand <tucker@gillbrand.com>
2021-07-30 08:40:51 -0700

    Initial Commit

---
+++ LocationHackwich/LocationHackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationHackwichApp.swift
+//  LocationHackwich
+//
+//  Created by Tucker(School) on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct LocationHackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
